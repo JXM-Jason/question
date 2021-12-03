24ef0f9ac36d9636c7e0d893f0593ef9cfec949e
JXM <1162782002@qq.com>
2021-12-03 20:50:12 +0800

    出问题了

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="nav">
-    <router-link to="/Labels">标签</router-link>|
+    <router-link to="/Labels"> 标签 </router-link>|
     <router-link to="/Money">记一笔</router-link> |
     <router-link to="/Statistics">统计</router-link>
   </div>
 </template>
 
 <script>
+import x from "@assets/icons/labels.svg";
+console.log(x);
 export default {};
 </script>
 
