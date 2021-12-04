d53ed9c9183229de10decea08c335f8730c42bbd
JXM <1162782002@qq.com>
2021-12-04 13:42:42 +0800

    书写nav的样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="nav">
-    <router-link to="/Labels"> 标签 </router-link>|
-    <router-link to="/Money">记一笔</router-link> |
-    <router-link to="/Statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/Labels" class="link">
+      <icon name="labels" />
+      标签
+    </router-link>
+    <router-link to="/Money" class="link">
+      <icon name="money" />
+      记一笔
+    </router-link>
+    <router-link to="/Statistics" class="link">
+      <icon name="statistics" />
+      统计
+    </router-link>
+  </nav>
 </template>
 
-<script>
-import x from "@assets/icons/labels.svg";
-console.log(x);
+<script lang="ts">
 export default {};
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.9);
+  font-size: 12px;
+  padding: 2px 0 0 0;
+  > .link {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    padding: 2px 0;
+    > .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,5 +1,4 @@
-import Vue, { VNode } from 'vue'
-
+import Vue, { VNode } from "vue";
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
@@ -7,7 +6,7 @@ declare global {
     // tslint:disable no-empty-interface
     interface ElementClass extends Vue {}
     interface IntrinsicElements {
-      [elem: string]: any
+      [elem: string]: any;
     }
   }
 }
